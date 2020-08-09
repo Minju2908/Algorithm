9fc1a3f12dcbed55faea97d7e24d7f8fc93fa82d
minju0329 <minj0329@gmail.com>
2020-08-10 03:39:57 +0900

    Level2

--- Level2/다리를 지나는 트럭.py
+++ Level2/다리를 지나는 트럭.py
@@ -1,26 +1,35 @@
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    waiting = [[i, 0]for i in truck_weights]
+    waiting = [[i,0]for i in truck_weights]
     bridge = []
-    while waiting:
-        truck = waiting.pop(0)
-        if sum([i[1] for i in bridge])+truck[0] <= weight:
-            bridge.append(truck)
+    while 1:
+        if waiting:
+            answer += 1
+            truck = waiting.pop(0)
+            if sum([i[0] for i in bridge])+truck[0] <= weight:
+                bridge.append(truck)
+                for i in bridge:
+                    i[1] += 1
+                for i in bridge:
+                    if i[1] == bridge_length:
+                        del bridge[0]
+            else:
+                waiting.insert(0,truck)
+                for i in bridge:
+                    i[1] += 1
+                for i in bridge:
+                    if i[1] == bridge_length:
+                        del bridge[0]
         else:
-            pass
+            while bridge:
+                answer+=1
+                for i in bridge:
+                    i[1] += 1
+                for i in bridge:
+                    if i[1] == bridge_length:
+                        del bridge[0]
+            break
 
-        for i in bridge:
-            i[1]+=1
-            if i[1]>bridge_length:
-                bridge.pop(0)
+    return answer+1
 
-    return answer
-
-
-
-bridge_length = 2
-weight = 10
-truck_weights = [7,4,5,6]
-
-a = solution(bridge_length, weight, truck_weights)
-print(a)
